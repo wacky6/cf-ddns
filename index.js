7db45ec20ee00f991568bed40d51adb6965c464f
wacky6.AriesMBP <416707889@qq.com>
2018-02-02 15:55:00 +0800

    improve reliability
    1. move on to async/await
    2. resolve dns, check against record content
    3. allow custom endpoint to bypass gfw

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
+const {promisify} = require('util')
 const _ipify = require('ipify')
+const ipify = promisify(_ipify)
 const parseDomain = require('parse-domain')
 const {error, warn, info, verbose} = require('./lib/logger')
-const koa = require('koa')
-const co = require('co')
 const API = require('./lib/cf-api')
+const resolve = require('./lib/resolver')
 const {floor, max} = Math
-
-function ipify() {
-    return new Promise( (resolve, reject) =>
-        _ipify( (err, ip) => err ? reject(err) : resolve(ip) )
-    )
-}
 
 module.exports = function CloudflareDDNS({
     key,      // CF API Key
     email,    // CF Email
     host,     // host name
     interval = 120,    // ip addr probe interval,
+    endpoint,    // cloudflare v4 endpoint
     logger: {
         error, warn, info, verbose, silly
     }
 }) {
-    let api = API(key, email)
+    let api = API(key, email, endpoint)
     let curIp = null
     let domain = parseDomain(host)
     if ( ! domain ) {
@@ -32,7 +28,7 @@ module.exports = function CloudflareDDNS({
     let domainId = `${domain.domain}.${domain.tld}`
     verbose(`top-level domain: ${domainId}`)
 
-    let cbk = () => co(function*(){
+    async function intervalFn() {
         let ip,
             type,
             ttl = max(120, interval),
@@ -40,21 +36,24 @@ module.exports = function CloudflareDDNS({
 
         // get public ip from ipify
         try {
-            ip = yield ipify()
-            verbose(`probed ip: ${ip}`)
+            ip = await ipify()
+            verbose(`  probed ip: ${ip}`)
             type = ip.indexOf(':') !== -1 ? 'AAAA' : 'A'
         } catch(err) {
             return warn(`fail to get ip: ${err.message}`)
         }
 
+        let currentRecords = await resolve(host)
+        let curIp = currentRecords[type] || currentRecords['AAAA'] || currentRecords['A']
+        verbose(`resolved ip: ${curIp}`)
+
         if (curIp === ip)
-            return verbose(`no change in ip`)
-        curIp = ip
+            return
 
         try {
             let body
             // get zone identifier
-            body = yield api(
+            body = await api(
                 'GET',
                 '/zones',
                 (req) => req.query({ name: domainId, status: 'active', match: 'all' })
@@ -64,31 +63,40 @@ module.exports = function CloudflareDDNS({
             verbose(`zone id: ${zoneId}`)
 
             // get record identifier
-            body = yield api(
+            body = await api(
                 'GET',
                 `/zones/${zoneId}/dns_records`,
                 (req) => req.query({ name: host })
             )
 
             let recordId = body.result[0] ? body.result[0].id : null
-            verbose(`record id: ${recordId}`)
+            let recordAddr = body.result[0] ? body.result[0].content : null
+            if (recordId) {
+                verbose(`record id: ${recordId}, addr: ${recordAddr}`)
+            } else {
+                verbose(`no record for host`)
+            }
 
-            // update or create DNS record
-            body = yield api(
-                recordId ? 'PUT' : 'POST',
-                recordId ? `/zones/${zoneId}/dns_records/${recordId}` : `/zones/${zoneId}/dns_records`,
-                (req) => req.send({ type, name: host, content: ip, ttl, proxied: false })
-            )
-
-            info(`dns record updated: ${host} = ${type} ${ip}`)
+            if (recordAddr !== ip) {
+                // update or create DNS record
+                body = await api(
+                    recordId ? 'PUT' : 'POST',
+                    recordId ? `/zones/${zoneId}/dns_records/${recordId}` : `/zones/${zoneId}/dns_records`,
+                    (req) => req.send({ type, name: host, content: ip, ttl, proxied: false })
+                )
+                info(`record updated: ${host} = ${type} ${ip}`)
+            } else {
+                info(`record matches probed ip, wait for dns propagation`)
+            }
 
         } catch(err) {
+            warn(err.stack)
             return warn(err.message)
         }
-    })
+    }
 
-    let itvl = setInterval(cbk, interval * 1000)
-    cbk()
+    let itvl = setInterval(intervalFn, interval * 1000)
+    intervalFn()
 
     return {
         stop() {
